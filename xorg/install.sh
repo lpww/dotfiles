ac8bdd9cc6de0df1dc939edf458dea7bbd7bb6f6
thomas <thomas.treffry@nearform.com>
2018-08-12 18:25:53 +0200

    Add xorg

---
+++ xorg/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+dirname="$(dirname "$(readlink -f "$0")")"
+echo $dirname
+source "$dirname/../scripts/_shared.sh"
+
+src="$dirname/Xresources"
+dst="$HOME/.Xresources"
+_link "$src" "$dst"
+
+src="$dirname/colors.Xresources"
+dst="$HOME/.colors.Xresources"
+_link "$src" "$dst"
+
+src="$dirname/xorg.conf.d/90-monitor.conf"
+dst="/etc/X11/xorg.conf.d/90-monitor.conf"
+_sudo_link "$src" "$dst"
