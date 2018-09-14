c831ae3af53182c6a1bae37bb72f4ac138b6faa1
thomas <thomas.treffry@nearform.com>
2018-09-14 18:32:14 +0200

    Add xprofile

--- xorg/install.sh
+++ xorg/install.sh
@@ -4,6 +4,10 @@ dirname="$(dirname "$(readlink -f "$0")")"
 echo $dirname
 source "$dirname/../scripts/_shared.sh"
 
+src="$dirname/xprofile"
+dst="$HOME/.xprofile"
+_sudo_link "$src" "$dst"
+
 src="$dirname/Xresources"
 dst="$HOME/.Xresources"
 _link "$src" "$dst"
